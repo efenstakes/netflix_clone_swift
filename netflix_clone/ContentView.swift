71657e76a911ab7573b2d52c1c689659cab7f159
Efen Stakes <efenstakes101@gmail.com>
2021-10-07 10:05:43 +0300

    add tab bar

--- netflix_clone/ContentView.swift
+++ netflix_clone/ContentView.swift
@@ -9,8 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        TabView {
+            
+            HomePage()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "house.fill")
+                            .resizable()
+                            .frame(width: 16, height: 16, alignment: .center)
+                            .foregroundColor(.white)
+                        Text("Home")
+                            .font(.caption)
+                            .foregroundColor(.white)
+                    }
+                }
+            
+            
+            Text("Coming Soon Page")
+                .padding()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "play.tv")
+                            .resizable()
+                            .frame(width: 16, height: 16, alignment: .center)
+                            .foregroundColor(.white)
+                        Text("Coming Soon")
+                            .font(.caption)
+                            .foregroundColor(.white)
+                    }
+                }
+            
+            
+            Text("Downloads Page")
+                .padding()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "arrow.down.circle")
+                            .resizable()
+                            .frame(width: 16, height: 16, alignment: .center)
+                            .foregroundColor(.white)
+                        Text("Downloads")
+                            .font(.caption)
+                            .foregroundColor(.white)
+                    }
+                }
+            
+            
+        }
+        .accentColor(.white)
+//        .onAppear(perform: { (){ print("set color") } })
+        
     }
 }
 
