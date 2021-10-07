d06d24e97af2d7362fc9e6bb55cd61088ec4afce
Efen Stakes <efenstakes101@gmail.com>
2021-10-07 09:27:09 +0300

    add home page app bar

--- netflix_clone/home_page.swift
+++ netflix_clone/home_page.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+
+struct HomePage: View {
+    @StateObject private var filmsViewModel: FilmsViewModel = FilmsViewModel()
+    
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            
+            Color.black.opacity(0.7).ignoresSafeArea(.all)
+            
+            
+            ScrollView {
+                
+                // top bar
+                HStack(alignment: .center, spacing: 12) {
+                    
+                    Text("NetFlix")
+                        .foregroundColor(.red)
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "tv")
+                        .resizable()
+                        .frame(width: 20, height: 20, alignment: .center)
+                        .foregroundColor(.white)
+                    
+                    Image(systemName: "person.crop.circle")
+                        .resizable()
+                        .frame(width: 20, height: 20, alignment: .center)
+                        .foregroundColor(.white)
+                    
+                }
+                .padding(
+                    EdgeInsets(top: 12, leading: 16, bottom: 12, trailing: 16)
+                )
+                
+                
+            }
+            
+        }
+    }
+}
