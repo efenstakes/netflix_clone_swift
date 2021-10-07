92bd9ba5cee8ebc4321b8493b95e4454f2a96356
kipchumba26 <cynthia.kipchumba@nativecoders.se>
2021-10-07 08:17:33 +0300

    add view model to contain app data

--- netflix_clone/view_models/films_view_model.swift
+++ netflix_clone/view_models/films_view_model.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+
+
+class FilmsViewModel: ObservableObject {
+    @Published var films = [Film]()
+    
+    
+    init() {
+        self.films = filmsData
+    }
+    
+}
