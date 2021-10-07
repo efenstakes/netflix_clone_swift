f46fff9d5b7e16f9fefd3a055222bad97640a945
kipchumba26 <cynthia.kipchumba@nativecoders.se>
2021-10-07 08:17:10 +0300

    add film model and some test data

--- netflix_clone/models/film.swift
+++ netflix_clone/models/film.swift
@@ -6,3 +6,81 @@
 //
 
 import Foundation
+
+
+enum FilmType {
+    case movie, series
+}
+enum FilmGenre {
+    case drama, comedy, documentary, fiction, nonFiction
+}
+
+struct Film: Identifiable {
+    var id: String
+    var name: String
+    var type: FilmType = .movie
+    var genre: FilmGenre = .drama
+    var image: String
+}
+
+
+let filmsData: [Film] = [
+
+    Film(
+        id: UUID().uuidString,
+        name: "Lupin",
+        type: .series,
+        genre: .drama,
+        image: "2"
+    ),
+    Film(
+        id: UUID().uuidString,
+        name: "Friends",
+        type: .series,
+        genre: .comedy,
+        image: "3"
+    ),
+    Film(
+        id: UUID().uuidString,
+        name: "Sonic HedgeHog",
+        type: .movie,
+        genre: .drama,
+        image: "4"
+    ),
+    Film(
+        id: UUID().uuidString,
+        name: "Family Reunion",
+        type: .series,
+        genre: .comedy,
+        image: "5"
+    ),
+    Film(
+        id: UUID().uuidString,
+        name: "How I Met Your Mother",
+        type: .series,
+        genre: .comedy,
+        image: "6"
+    ),
+    Film(
+        id: UUID().uuidString,
+        name: "Blood & Water",
+        type: .series,
+        genre: .drama,
+        image: "7"
+    ),
+    Film(
+        id: UUID().uuidString,
+        name: "Good Doctor",
+        type: .series,
+        genre: .drama,
+        image: "8"
+    ),
+    Film(
+        id: UUID().uuidString,
+        name: "Lucifer",
+        type: .series,
+        genre: .drama,
+        image: "9"
+    )
+    
+]
