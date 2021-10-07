b2bca528257b09851fda08dbd63ca31a72df6061
kipchumba26 <cynthia.kipchumba@nativecoders.se>
2021-10-07 07:34:28 +0300

    Initial Commit

---
+++ netflix_clone/netflix_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflix_cloneApp.swift
+//  netflix_clone
+//
+//  Created by MAC on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct netflix_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ netflix_cloneUITests/netflix_cloneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  netflix_cloneUITestsLaunchTests.swift
+//  netflix_cloneUITests
+//
+//  Created by MAC on 10/7/21.
+//
+
+import XCTest
+
+class netflix_cloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
